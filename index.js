e1d9bab5d539f46b11880ad20bb306b388af9745
Anthony Bui <anthonybui@gmail.com>
2016-06-20 15:45:46 -0400

    Update index

--- index.js
+++ index.js
@@ -13,8 +13,10 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.get('/test', function(req, res) {
+  return res.status(200).json({ foo: 'bar' });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
