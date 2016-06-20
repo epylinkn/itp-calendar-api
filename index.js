a2cbfe5e14e905138591ea091a168fd883326e73
Anthony Bui <anthonybui@gmail.com>
2016-06-20 17:05:27 -0400

    Just titles and tags

--- index.js
+++ index.js
@@ -26,40 +26,22 @@ app.get('/calendar', function(req, res) {
       // Next, we'll utilize the cheerio library on the returned html which will essentially give us jQuery functionality
       var $ = cheerio.load(html);
 
-      var sessions = {};
-      var dateHeaders = $('.dateHeader');
-      dateHeaders.each(function(i, elem) {
-        //console.log($(elem).text());
-        var date = $(elem).text();
-        sessions[date] = {};
-        for (var i = 0; i < 24; i+=.5) {
-          sessions[date][i] = 0;
-        }
-        var times = $(elem).nextAll('.sessionListItem');
-        times.each(function(i, elem) {
-          //console.log($(elem).find('.sessionInfo b').text());
-          var time_string = $(elem).find('.sessionInfo b').text();
-          var start_time = time_string.split('-')[0];
-          var start_time_ampm = start_time.indexOf('am');
-          start_time = start_time.replace(/\D/, '').replace('30', '.5');
-          if (start_time_ampm === -1) {
-            // we're in pm
-            start_time = parseFloat(start_time) + 12;
-          }
+      var sessions = [];
+      var $sessions = $('.sessionListItem');
 
-          var end_time = time_string.split('-')[1];
-          var end_time_ampm = end_time.indexOf('am');
-          end_time = end_time.replace(/\D/, '').replace('30', '.5');
-          if (end_time_ampm === -1) {
-            // we're in pm
-            end_time = parseFloat(end_time) + 12;
-          }
-          for (var i = start_time; i < end_time; i += 0.5) {
-            sessions[date][i] += 1;
-          }
+      console.log($sessions);
 
-          //console.log('Breakdown', time_string, " : ", start_time, " : ", end_time);
-        });
+      $sessions.each(function(i, elem) {
+          var session = {};
+          session.title = $(elem).find('h3 a').text();
+
+          var tags = [];
+          $(elem).find('.sessionTags a').each(function(i, elem) {
+              tags.push($(elem).text());
+          })
+          session.tags = tags;
+
+          sessions.push(session);
       });
       return res.status(200).json(sessions);
     })
